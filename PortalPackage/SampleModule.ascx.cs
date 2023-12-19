ed400200d0f0b4b22bd67d38302cac041df064e1
365nguyen <96913748+365nguyen@users.noreply.github.com>
2023-12-19 14:22:22 -0500

    fix issues

--- PortalPackage/SampleModule.ascx.cs
+++ PortalPackage/SampleModule.ascx.cs
@@ -65,43 +65,8 @@ namespace PortalPackage
 
             Console.WriteLine("Sum: " + sum);
         }
-        public static void SampleLegacyCode2()
-        {
-            // Bug: Division by zero
-            int x = 5;
-            int y = 0;
-            int result = x / y; // This will throw a divide by zero exception
+        
 
-            // Code Smell: Using StringBuilder unnecessarily
-            string message = "Hello, ";
-            message += "world!"; // Using string concatenation is more efficient in this case
-
-            // Vulnerability: SQL Injection
-            string userInput = "John";
-            string sqlQuery = "SELECT * FROM Users WHERE Name = '" + userInput + "'"; // Vulnerable to SQL injection
-
-            // Security Hotspot: Storing Passwords in Plain Text
-            string password = "MySecretPassword";
-            Console.WriteLine("Password: " + password); // Storing and displaying passwords in plain text is insecure
-
-            // Bug: Unhandled exception
-            int[] numbers = { 1, 2, 3 };
-            int index = 5;
-            try
-            {
-                int value = numbers[index]; // Accessing an out-of-bounds index
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
-
-            // Code Smell: Lack of comments and unclear variable names
-            int a = 10; // What does 'a' represent?
-            int b = 20; // What does 'b' represent?
-            int sum = a + b;
-
-            Console.WriteLine("Sum: " + sum);
-        }
+        
     }
 }
